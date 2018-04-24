71148c344636b2e04be9338e7cad7d1bb0885a46
Sahil004 <sahilkrsingh004@gmail.com>
2018-04-24 14:54:41 +0530

    '.'

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const webdriver = require('selenium-webdriver'),
      proxy = require('selenium-webdriver/lib/proxy');
 async function asycall(){
 	var driver = await new webdriver.Builder()
-	.withCapabilities(webdriver.Capabilities.firefox())
+	.withCapabilities(webdriver.Capabilities.chrome())
 	.setProxy(proxy.manual({
 		ftp:'87.98.174.157:3128',
 		http: '87.98.174.157:3128',
 		https:'87.98.174.157:3128',
 	}))
 	.build()
-	await driver.get("https://whatismyipaddress.com/").catch((err)=>{
+	await driver.get("http://www.sustainablesites.org/").catch((err)=>{
 		console.log(err);
 	})
 	await driver.sleep(100000)
